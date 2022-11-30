469c99aa7b371e299a29dac6ba6c6fdf250ed1bb
Gajanan <kendreg27@gmail.com>
2022-11-30 14:55:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+//import ResultCard from "./ResultCard/ResultCard";
+import Show from "./ResultCard/Show";
+import Actor from "./ResultCard/Actor";
 
 function App() {
+  const [type, setType] = useState("");
+  const [searchString, setSearchString] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const notfound = 'https://png.pngtree.com/png-clipart/20200225/original/pngtree-error-page-not-found-concept-illustration-flat-design-with-people-this-png-image_5276232.jpg';
+
+  useEffect(() => {
+    getSearchResults();
+  }, [type, searchString]);
+
+  const getSearchResults = async () => {
+    if (type === "" || searchString === "") {
+      return;
+    }
+
+    setIsLoading(true);
+    const response = await fetch(
+      `https://api.tvmaze.com/search/${type}?q=${searchString}`
+    );
+    const data = await response.json();
+    setIsLoading(false);
+    console.log(`data ${searchString}`, data);
+    setSearchResults(data);
+  };
+
+  const onActorChange = () => {
+    setType("people");
+  };
+
+  const onShowsChange = () => {
+    setType("shows");
+  };
+
+  const onSearchChange = (event) => {
+    setSearchString(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="grey-container">
+        <h1>TVmaze</h1>
+        <h2>Search your favorite shows</h2>
+
+        <input
+          type="radio"
+          id="actor"
+          name="type"
+          checked={type === "people"}
+          onChange={onActorChange}
+        />
+        <label htmlFor="actor">Actor</label>
+
+        <input
+          type="radio"
+          id="shows"
+          name="type"
+          checked={type === "shows"}
+          onChange={onShowsChange}
+        />
+        <label htmlFor="shows">Shows</label>
+
+        <div className="input-wrapper">
+          <input
+            placeholder="eg. Friends..."
+            value={searchString}
+            onChange={onSearchChange}
+          />
+        </div>
+        {isLoading && <div className="loader">Loading...</div>}
+        {searchResults.length === 0 && (
+          <div className="no-results">No results found</div>
+        )}
+
+      </div>
+      <div className="Homepage_list">
+        {
+          (type === 'people')
+            ? (
+              searchResults.map((result, index) => (
+                <Actor
+                  key={index}
+                  imageUrl={result?.person?.image?.medium || notfound}
+                  name={result?.person?.name}
+                //description={result.show.summary}
+                //rating={result.show.rating.average}
+                />
+              ))
+            )
+            : (
+              searchResults.map((result, index) => (
+                <Show
+                  key={result?.show?.id || index}
+                  imageUrl={result?.show?.image?.medium || notfound}
+                  name={result?.show?.name}
+                  description={result?.show?.summary}
+                  rating={result?.show?.rating?.average}
+                />
+              ))
+            )
+        }
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/ResultCard/Actor.js
@@ -0,0 +1,25 @@
+import "./style.css";
+// import notfound from '../notfound.png';
+const Actor = (props) => {
+    const { imageUrl, name } = props;
+
+
+    return (
+        <div className="card-wrapper">
+            <img className="card-image" src={imageUrl} alt="Card cover" />
+            <div className="bottom-container">
+                <h6>{name}</h6>
+
+                {/* <div
+            dangerouslySetInnerHTML={{
+              __html: description,
+            }}
+          />
+          <div>{rating}</div> */}
+
+            </div>
+        </div>
+    );
+};
+
+export default Actor;
