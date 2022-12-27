74c609dcc6c53fab57c33aa75a4ca59d7e86ae47
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2022-12-27 06:56:42 +0530

    Update Show.js

--- src/ResultCard/Show.js
+++ src/ResultCard/Show.js
@@ -1,24 +1,58 @@
 import "./style.css";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 const Show = (props) => {
 
     const { imageUrl, name, description, rating } = props;
 
+    const [flag, setFlag] = useState(false);
+
+    const handleModal = () => {
+        setFlag(!flag);
+    }
 
     return (
         <div className="card-wrapper">
             <img className="card-image" src={imageUrl} alt="Card cover" />
             <div className="bottom-container">
                 <h6>{name}</h6>
-                {/* <div
+
+                <div>
+                    <Button onClick={handleModal} >Description</Button>
+                    <Modal show={flag} >
+
+                        <Modal.Header>
+                            <h3>Summary of: {name}</h3>
+                        </Modal.Header>
+
+                        <Modal.Body>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: description,
+                                }}
+                            />
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={handleModal}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+
+                {/* 
+                 <div
                     dangerouslySetInnerHTML={{
                         __html: description,
                     }}
-                /> */}
+                    style={{ color: "red" }}
+                />  */}
+
                 <div>{rating}</div>
             </div>
         </div>
     );
 };
-
 export default Show;
+
